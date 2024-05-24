a4f0b724a788997890c6c5c76c722cb873b0e2a4
Delta-Leiras <138947739+Delta-Leiras@users.noreply.github.com>
2024-05-24 15:05:12 -0300

    Errei o caminho do component na hora de importar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Titulo from './components/Titulo';
+import Titulo from './components/Titulo/index.js';
 
 
 const App = () => 
