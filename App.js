a0a918c93cafddb64e3a8530b14e0063484602ab
Delta-Leiras <138947739+Delta-Leiras@users.noreply.github.com>
2024-05-24 14:56:10 -0300

    Primeira tentativa de criar components e estilizá-los

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Titulo from './components/Titulo';
 
-export default function App() 
-{
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const App = () => 
 <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Titulo></Titulo>
+      <Text>O bagui de cima é o título</Text>
       <StatusBar style="auto" />
  </View>;
 
-export default App;*/
+export default App;
 
 const styles = StyleSheet.create
 ({
